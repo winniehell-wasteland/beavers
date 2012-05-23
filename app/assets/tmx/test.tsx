8cf562aaf127fe7fb85852f1dd343cf471ebf892
wintermadnezz <max.aj.franke@web.de>
2012-05-23 16:21:31 +0200

    Soldier, TmxMap

---
+++ app/assets/tmx/test.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="64" tileheight="64" spacing="2" margin="2">
+ 
+ <image source="gfx/tileset.png" width="800" height="600"/>
+</tileset>
