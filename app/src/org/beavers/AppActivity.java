7a9522ac763bad10b255c2e786c2468ba2c72b7e
winniehell <git@winniehell.de>
2012-05-25 01:48:02 +0200

    fix menu, remove old stuff

--- app/src/org/beavers/AppActivity.java
+++ app/src/org/beavers/AppActivity.java
@@ -1,5 +1,6 @@
 package org.beavers;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.anddev.andengine.engine.Engine;
@@ -8,169 +9,145 @@ import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.scene.background.ColorBackground;
-import org.anddev.andengine.entity.scene.menu.MenuScene;
-import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
-import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
 import org.anddev.andengine.entity.util.FPSLogger;
-import org.anddev.andengine.opengl.font.Font;
-import org.anddev.andengine.opengl.texture.Texture;
-import org.anddev.andengine.opengl.texture.TextureOptions;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.anddev.andengine.opengl.view.RenderSurfaceView;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 import org.beavers.communication.Client;
 import org.beavers.communication.CustomDTNClient;
 import org.beavers.communication.CustomDTNDataHandler;
 import org.beavers.communication.Server;
-import org.beavers.gameplay.GameScene;
 import org.beavers.gameplay.GameID;
 import org.beavers.gameplay.GameInfo;
+import org.beavers.gameplay.GameScene;
 import org.beavers.gameplay.PlayerID;
+import org.beavers.ui.GameListView;
 
+import android.os.Bundle;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import de.tubs.ibr.dtn.api.DTNClient.Session;
 import de.tubs.ibr.dtn.api.Registration;
 import de.tubs.ibr.dtn.api.ServiceNotAvailableException;
 import de.tubs.ibr.dtn.api.SessionDestroyedException;
 
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.database.DataSetObserver;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.view.Display;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.Surface;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+public class AppActivity extends BaseGameActivity {
 
-public class AppActivity extends BaseGameActivity {	
-
-	private ListView serverListView;
-
-	private ViewGroup frameLayout;
-	public AppActivity() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	protected void onCreate(Bundle pSavedInstanceState) {
-		super.onCreate(pSavedInstanceState);
-
+	public AppActivity()
+	{
 		playerID = new PlayerID(UUID.randomUUID().toString());
-		
+
 		client = new Client(this);
 		server = new Server(this);
 
+		runningGames = new ArrayList<GameInfo>();
+
+		runningGames.add(new GameInfo(new PlayerID("server1"), new GameID("game1")));
+		runningGames.add(new GameInfo(new PlayerID("server2"), new GameID("game2")));
+		runningGames.add(new GameInfo(new PlayerID("server3"), new GameID("game3")));
+		runningGames.add(new GameInfo(new PlayerID("server4"), new GameID("game4")));
+	}
+
+	@Override
+	protected void onCreate(final Bundle pSavedInstanceState) {
+		super.onCreate(pSavedInstanceState);
+
 		dtnClient = new CustomDTNClient(this);
 		dtnDataHandler = new CustomDTNDataHandler(dtnClient, server, client);
-		
+
         dtnClient.setDataHandler(dtnDataHandler);
-                
+
         try {
         	final Registration registration = new Registration("game/server");
 
         	registration.add(Server.GROUP_EID);
         	registration.add(Client.GROUP_EID);
-        	
+
 			dtnClient.initialize(registration);
-		} catch (ServiceNotAvailableException e) {
+		} catch (final ServiceNotAvailableException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
+	public boolean onCreateOptionsMenu(final Menu menu) {
+	    final MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.main_menu, menu);
 	    return true;
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// unregister at the daemon
 		dtnClient.unregister();
 
 		dtnDataHandler.stop();
-		
+
 		// destroy DTN client
 		dtnClient.terminate();
-		
+
 		super.onDestroy();
 	}
 
 	@Override
 	public Engine onLoadEngine() {
-		
-        Display display = getWindowManager().getDefaultDisplay();
-       
+
+        final Display display = getWindowManager().getDefaultDisplay();
+
         ScreenOrientation orientation;
-        
-        int rotation = display.getRotation();
+
+        final int rotation = display.getRotation();
         if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
         	orientation = ScreenOrientation.LANDSCAPE;
         } else {
         	orientation = ScreenOrientation.PORTRAIT;
         }
-		
-		this.camera = new Camera(0, 0, display.getWidth(), display.getHeight());
-		
-		return new Engine(new EngineOptions(true, orientation, 
-				new RatioResolutionPolicy(display.getWidth(), display.getHeight()), this.camera));
+
+		camera = new Camera(0, 0, display.getWidth(), display.getHeight());
+
+		return new Engine(new EngineOptions(true, orientation,
+				new RatioResolutionPolicy(display.getWidth(), display.getHeight()), camera));
 	}
-	
+
 	@Override
 	public void onLoadResources() {
-		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-		
-		fontTexture = new BitmapTextureAtlas(512, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);		
-		menuFont = new Font(fontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 100, true, Color.BLACK);
-		
-		getTextureManager().loadTexture(this.fontTexture);
-		getFontManager().loadFont(menuFont);
+
 	}
 
 	@Override
 	public Scene onLoadScene() {
 		mEngine.registerUpdateHandler(new FPSLogger());
 		gameScene = new GameScene(this);
-		
+
 		return gameScene;
 	}
 
 	@Override
 	public void onLoadComplete() {
-		
+
 	}
 
 	@Override
 	public boolean onKeyDown(final int pKeyCode, final KeyEvent pEvent) {
-		
+
 		if(pKeyCode == KeyEvent.KEYCODE_BACK && pEvent.getAction() == KeyEvent.ACTION_DOWN) {
-			
-			if(serverListView.getParent() == frameLayout)
+
+			if(runningGamesView.getParent() == frameLayout)
 		    {
-				this.finish();
+				finish();
 		    }
 			else
 			{
 		    	frameLayout.removeAllViews();
-			    frameLayout.addView(serverListView);
+			    frameLayout.addView(runningGamesView);
 			}
-			
+
 			return true;
 		} else {
 			return super.onKeyDown(pKeyCode, pEvent);
@@ -178,34 +155,43 @@ public class AppActivity extends BaseGameActivity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(final MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.menu_start_game:
-			
+
 		    if(mRenderSurfaceView.getParent() != frameLayout)
 		    {
 		    	frameLayout.removeAllViews();
-		    	
+
 			    final FrameLayout.LayoutParams surfaceViewLayoutParams = new FrameLayout.LayoutParams(super.createSurfaceViewLayoutParams());
-			    frameLayout.addView(this.mRenderSurfaceView, surfaceViewLayoutParams);
+			    frameLayout.addView(mRenderSurfaceView, surfaceViewLayoutParams);
 		    }
-		    						
-			GameInfo newGame = new GameInfo(getPlayerID(), new GameID(UUID.randomUUID().toString()));
+
+			final GameInfo newGame = new GameInfo(getPlayerID(), new GameID(UUID.randomUUID().toString()));
 			server.initiateGame(newGame);
-			
+
 			return true;
 		case R.id.menu_join_game:
-			
-		    if(serverListView.getParent() != frameLayout)
+
+		    if(announcedGamesView.getParent() != frameLayout)
 		    {
 		    	frameLayout.removeAllViews();
-			    frameLayout.addView(serverListView);
+			    frameLayout.addView(announcedGamesView);
 		    }
-			
+
+			return true;
+		case R.id.menu_running_games:
+
+		    if(runningGamesView.getParent() != frameLayout)
+		    {
+		    	frameLayout.removeAllViews();
+			    frameLayout.addView(runningGamesView);
+		    }
+
 			return true;
 		case R.id.menu_quit:
-			this.finish();
-			
+			finish();
+
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
@@ -219,101 +205,48 @@ public class AppActivity extends BaseGameActivity {
 	public Client getClient() {
 		return client;
 	}
-	
+
 	public Session getDTNSession() throws SessionDestroyedException, InterruptedException {
 		return dtnClient.getSession();
 	}
-	
+
 	public Server getServer() {
 		return server;
 	}
-	
+
 	@Override
 	protected void onSetContentView() {
-	    frameLayout = new FrameLayout(this);
-	    final FrameLayout.LayoutParams frameLayoutLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.FILL_PARENT);
-
 	    mRenderSurfaceView = new RenderSurfaceView(this);
 	    mRenderSurfaceView.setRenderer(mEngine);
-	    	    	    
-	    serverListView = new ListView(this);
-	    serverListView.setPadding(20, 10, 10, 10);
-	    
-	    serverListView.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.FILL_PARENT,
-                LinearLayout.LayoutParams.FILL_PARENT
-            ));
-	    	    
-	    serverListView.setAdapter(new ListViewAdapter());
 
-	    this.setContentView(frameLayout, frameLayoutLayoutParams);
+	    assert client != null;
+	    announcedGamesView = new GameListView(this, client.announcedGames);
+
+	    runningGamesView = new GameListView(this, runningGames);
+
+	    frameLayout = new FrameLayout(this);
+	    final FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+	    		FrameLayout.LayoutParams.FILL_PARENT,
+	    		FrameLayout.LayoutParams.FILL_PARENT);
+	    setContentView(frameLayout, layoutParams);
+
+	    frameLayout.addView(runningGamesView);
 	}
-	
-	class ListViewAdapter extends BaseAdapter {
-   	 
-   	 private LayoutInflater mInflater = LayoutInflater.from(AppActivity.this);
 
-   	 public int getCount() {
-   		 if(client == null)
-   		 {
-   			 return 0;
-   		 }
-   		 
-   	  return client.announcedGames.size();
-   	 }
+	private final PlayerID playerID;
 
-   	 public Object getItem(int position) {
-   		 if(client == null)
-   		 {
-   			 return null;
-   		 }
-   		 
-   	  return client.announcedGames.get(position);
-   	 }
+	private final Client client;
+	private final Server server;
 
-   	 public long getItemId(int position) {
-   	  return position;
-   	 }
-
-   	 public View getView(int position, View convertView, ViewGroup parent) {
-   	  ViewHolder holder;
-   	  if (convertView == null) {
-   	   convertView = mInflater.inflate(R.layout.custom_row_view, null);
-   	   holder = new ViewHolder();
-   	   holder.txtName = (TextView) convertView.findViewById(R.id.name);
-   	   holder.txtServer = (TextView) convertView.findViewById(R.id.server);
-   	   holder.txtState = (TextView) convertView.findViewById(R.id.state);
-
-   	   convertView.setTag(holder);
-   	  } else {
-   	   holder = (ViewHolder) convertView.getTag();
-   	  }
-   	  
-   	  holder.txtName.setText(client.announcedGames.get(position).getID().toString());
-   	  holder.txtServer.setText(client.announcedGames.get(position).getServer().toString());
-   	  holder.txtState.setText("Waiting...");
-
-   	  return convertView;
-   	 }
-
-   	 class ViewHolder {
-   	  TextView txtName;
-   	  TextView txtServer;
-   	  TextView txtState;
-   	 }	
-   };
-	
-	private PlayerID playerID;
-
-	private Client client;
-	private Server server;
-	
 	private CustomDTNClient dtnClient;
 	private CustomDTNDataHandler dtnDataHandler;
+
+	private ViewGroup frameLayout;
+	private GameListView announcedGamesView;
+	private GameListView runningGamesView;
 
 	private Camera camera;
 	private GameScene gameScene;
 
-	private Texture fontTexture;
-	private Font menuFont;
+	private final ArrayList<GameInfo> runningGames;
 }
---
+++ app/src/org/beavers/ui/GameListView.java
@@ -0,0 +1,89 @@
+package org.beavers.ui;
+
+import java.util.ArrayList;
+
+import org.beavers.R;
+import org.beavers.gameplay.GameInfo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class GameListView extends ListView {
+
+	public GameListView(final Context pContext, final ArrayList<GameInfo> pList) {
+		super(pContext);
+
+		list = pList;
+
+		setPadding(20, 10, 10, 10);
+		setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.FILL_PARENT
+				));
+
+		setAdapter(new ListViewAdapter(pContext));
+	}
+
+	final private ArrayList<GameInfo> list;
+
+
+	class ListViewAdapter extends BaseAdapter {
+
+		public ListViewAdapter(final Context pContext) {
+			 inflater = LayoutInflater.from(pContext);
+		}
+
+		@Override
+		public int getCount() {
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(final int position) {
+			return list.get(position);
+		}
+
+		@Override
+		public long getItemId(final int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(final int position, View convertView, final ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.custom_row_view, null);
+				holder = new ViewHolder();
+				holder.txtName = (TextView) convertView.findViewById(R.id.name);
+				holder.txtServer = (TextView) convertView.findViewById(R.id.server);
+				holder.txtState = (TextView) convertView.findViewById(R.id.state);
+
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+
+			final GameInfo item = (GameInfo) getItem(position);
+
+			holder.txtName.setText(item.getID().toString());
+			holder.txtServer.setText(item.getServer().toString());
+			holder.txtState.setText("Unknown...");
+
+			return convertView;
+		}
+
+		class ViewHolder {
+			TextView txtName;
+			TextView txtServer;
+			TextView txtState;
+		}
+
+		private final LayoutInflater inflater;
+	}
+}
