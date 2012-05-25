a7d06f190d1647e25cd466820ae4e5ac1e35b915
winniehell <git@winniehell.de>
2012-05-25 02:02:45 +0200

    cleaned up

--- app/src/org/beavers/AppActivity.java
+++ app/src/org/beavers/AppActivity.java
@@ -23,16 +23,11 @@ import org.beavers.communication.Client;
 import org.beavers.communication.CustomDTNClient;
 import org.beavers.communication.CustomDTNDataHandler;
 import org.beavers.communication.Server;
-import org.beavers.gameplay.GameScene;
 import org.beavers.gameplay.GameID;
 import org.beavers.gameplay.GameInfo;
+import org.beavers.gameplay.GameScene;
 import org.beavers.gameplay.PlayerID;
 import org.beavers.ui.Menu;
-
-import de.tubs.ibr.dtn.api.DTNClient.Session;
-import de.tubs.ibr.dtn.api.Registration;
-import de.tubs.ibr.dtn.api.ServiceNotAvailableException;
-import de.tubs.ibr.dtn.api.SessionDestroyedException;
 
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -43,110 +38,115 @@ import android.view.Display;
 import android.view.KeyEvent;
 import android.view.Surface;
 import android.widget.Toast;
+import de.tubs.ibr.dtn.api.DTNClient.Session;
+import de.tubs.ibr.dtn.api.Registration;
+import de.tubs.ibr.dtn.api.ServiceNotAvailableException;
+import de.tubs.ibr.dtn.api.SessionDestroyedException;
 
-public class AppActivity extends BaseGameActivity implements IOnMenuItemClickListener {	
+public class AppActivity extends BaseGameActivity implements IOnMenuItemClickListener {
 
 	public AppActivity() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	protected void onCreate(Bundle pSavedInstanceState) {
+
+	@Override
+	protected void onCreate(final Bundle pSavedInstanceState) {
 		super.onCreate(pSavedInstanceState);
 
 		playerID = new PlayerID(UUID.randomUUID().toString());
-		
+
 		client = new Client(this);
 		server = new Server(this);
 
 		dtnClient = new CustomDTNClient(this);
 		dtnDataHandler = new CustomDTNDataHandler(dtnClient, server, client);
-		
+
         dtnClient.setDataHandler(dtnDataHandler);
-                
+
         try {
         	final Registration registration = new Registration("game/server");
 
         	registration.add(Server.GROUP_EID);
         	registration.add(Client.GROUP_EID);
-        	
+
 			dtnClient.initialize(registration);
-		} catch (ServiceNotAvailableException e) {
+		} catch (final ServiceNotAvailableException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// unregister at the daemon
 		dtnClient.unregister();
 
 		dtnDataHandler.stop();
-		
+
 		// destroy DTN client
 		dtnClient.terminate();
-		
+
 		super.onDestroy();
 	}
 
 	@Override
 	public Engine onLoadEngine() {
-		
-        Display display = getWindowManager().getDefaultDisplay();
-       
+
+        final Display display = getWindowManager().getDefaultDisplay();
+
         ScreenOrientation orientation;
-        
-        int rotation = display.getRotation();
+
+        final int rotation = display.getRotation();
         if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
         	orientation = ScreenOrientation.LANDSCAPE;
         } else {
         	orientation = ScreenOrientation.PORTRAIT;
         }
-		
-		this.camera = new SmoothCamera(0, 0, display.getWidth(), display.getHeight(), 2*display.getWidth(), 2*display.getHeight(),0);
-		
-		return new Engine(new EngineOptions(true, orientation, 
-				new RatioResolutionPolicy(display.getWidth(), display.getHeight()), this.camera));
+
+		camera = new SmoothCamera(0, 0, display.getWidth(), display.getHeight(), 2*display.getWidth(), 2*display.getHeight(),0);
+
+		return new Engine(new EngineOptions(true, orientation,
+				new RatioResolutionPolicy(display.getWidth(), display.getHeight()), camera));
 	}
-	
+
 	@Override
 	public void onLoadResources() {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-		
-		fontTexture = new BitmapTextureAtlas(512, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);		
+
+		fontTexture = new BitmapTextureAtlas(512, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		menuFont = new Font(fontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 100, true, Color.BLACK);
-		
-		getTextureManager().loadTexture(this.fontTexture);
+
+		getTextureManager().loadTexture(fontTexture);
 		getFontManager().loadFont(menuFont);
 	}
 
 	@Override
 	public Scene onLoadScene() {
-		this.mEngine.registerUpdateHandler(new FPSLogger());
+		mEngine.registerUpdateHandler(new FPSLogger());
 
-		this.mainScene = new Scene();
-		this.mainScene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+		mainScene = new Scene();
+		mainScene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
 
-		this.menuScene = new Menu(this.camera, this, menuFont);
-		this.gameScene = new GameScene(this);
-		
-		this.mainScene.setChildScene(this.menuScene);
-		
+		menuScene = new Menu(camera, this, menuFont);
+		gameScene = new GameScene(this);
+
+		mainScene.setChildScene(menuScene);
+
 		return mainScene;
 	}
 
 	@Override
 	public void onLoadComplete() {
-		
+
 	}
 
 	@Override
 	public boolean onKeyDown(final int pKeyCode, final KeyEvent pEvent) {
-		
+
 		if(pKeyCode == KeyEvent.KEYCODE_MENU && pEvent.getAction() == KeyEvent.ACTION_DOWN) {
-			if(this.mainScene.getChildScene() == this.gameScene) {
-				this.gameScene.back();
-				this.mainScene.setChildScene(this.menuScene);
+			if(mainScene.getChildScene() == gameScene) {
+				gameScene.back();
+				mainScene.setChildScene(menuScene);
 			}
 			return true;
 		} else {
@@ -155,17 +155,17 @@ public class AppActivity extends BaseGameActivity implements IOnMenuItemClickLis
 	}
 
 	@Override
-	public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
-			float pMenuItemLocalX, float pMenuItemLocalY) {
+	public boolean onMenuItemClicked(final MenuScene pMenuScene, final IMenuItem pMenuItem,
+			final float pMenuItemLocalX, final float pMenuItemLocalY) {
 		switch(pMenuItem.getID()) {
 		case Menu.START:
 			Toast.makeText(this, "Start game", Toast.LENGTH_SHORT).show();
-			
-			this.menuScene.back();
-			this.mainScene.setChildScene(gameScene);
-			
-			GameInfo newGame = new GameInfo(getPlayerID(), new GameID(UUID.randomUUID().toString()));
-			
+
+			menuScene.back();
+			mainScene.setChildScene(gameScene);
+
+			final GameInfo newGame = new GameInfo(getPlayerID(), new GameID(UUID.randomUUID().toString()));
+
 			server.initiateGame(newGame);
 
 			return true;
@@ -183,9 +183,9 @@ public class AppActivity extends BaseGameActivity implements IOnMenuItemClickLis
 			return true;
 		case Menu.QUIT:
 			System.out.println("Quit game");
-			
-			this.finish();
-			
+
+			finish();
+
 			return true;
 		default:
 			return false;
@@ -193,7 +193,7 @@ public class AppActivity extends BaseGameActivity implements IOnMenuItemClickLis
 	}
 
 	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
+	public void onConfigurationChanged(final Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 
 		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
@@ -210,20 +210,20 @@ public class AppActivity extends BaseGameActivity implements IOnMenuItemClickLis
 	public Client getClient() {
 		return client;
 	}
-	
+
 	public Session getDTNSession() throws SessionDestroyedException, InterruptedException {
 		return dtnClient.getSession();
 	}
-	
+
 	public Server getServer() {
 		return server;
 	}
-	
+
 	private PlayerID playerID;
 
 	private Client client;
 	private Server server;
-	
+
 	private CustomDTNClient dtnClient;
 	private CustomDTNDataHandler dtnDataHandler;
 
