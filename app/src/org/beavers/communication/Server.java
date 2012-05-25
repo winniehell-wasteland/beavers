c5633960364d7f93f220ba10497b3cd3451f11c7
winniehell <git@winniehell.de>
2012-05-25 19:45:01 +0200

    introduce GameState and GameList

--- app/src/org/beavers/communication/Server.java
+++ app/src/org/beavers/communication/Server.java
@@ -9,68 +9,79 @@ import org.beavers.AppActivity;
 import org.beavers.gameplay.DecisionContainer;
 import org.beavers.gameplay.GameID;
 import org.beavers.gameplay.GameInfo;
+import org.beavers.gameplay.GameList;
+import org.beavers.gameplay.GameState;
 import org.beavers.gameplay.OutcomeContainer;
 import org.beavers.gameplay.PlayerID;
 
+import android.util.Log;
 import de.tubs.ibr.dtn.api.GroupEndpoint;
 
 public class Server {
 
 	public static final GroupEndpoint GROUP_EID = new GroupEndpoint("dtn://beavergame.dtn/server");
 
-	/**
-	 * @name lifetimes
-	 * @{
-	 */
-	private static final int DEFAULT_LIFETIME = 100;
-	private static final int ANNOUNCEMENT_LIFETIME = DEFAULT_LIFETIME;
-	/**
-	 * @}
-	 */
-	
+	public final GameList hostedGames;
+
 	public Server(final AppActivity pApp)
 	{
 		app = pApp;
+		hostedGames = new GameList();
 	}
-	
+
 	/**
 	 * level select, inform possible clients
 	 */
-	public void initiateGame(GameInfo pGame)
+	public void initiateGame(final GameInfo pGame)
 	{
+		if(hostedGames.contains(pGame))
+		{
+			Log.e("Server", "Game "+pGame+" already exists!");
+			return;
+		}
+
 		final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		final PrintStream stream = new PrintStream(buffer);
-		
+
 		try {
 			stream.println("ANNOUNCE");
 			stream.println(pGame.getServer().toString());
 			stream.println(pGame.getID().toString());
-		
+
 			app.getDTNSession().send(Client.GROUP_EID, ANNOUNCEMENT_LIFETIME, buffer.toString());
-		
+
 			buffer.close();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		pGame.setState(GameState.ANNOUNCED);
+		hostedGames.add(pGame);
 	}
-	
+
 	/**
 	 * player tries to join game
 	 * @param player
 	 */
-	public void addPlayer(GameInfo pGame, PlayerID pPlayer)
+	public void addPlayer(final GameInfo pGame, final PlayerID pPlayer)
 	{
 		// TODO wait for all players, check if game still available
-		
+
 		gameReady(pGame);
 	}
-	
+
 	/**
 	 * server tries to start game
 	 */
-	public void gameReady(GameInfo pGame)
+	public void gameReady(final GameInfo pGame)
 	{
+		if(!hostedGames.contains(pGame))
+		{
+			Log.e("Server", "Could not find game "+pGame+"!");
+			return;
+		}
+
 		final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		final PrintStream stream = new PrintStream(buffer);
 
@@ -78,21 +89,29 @@ public class Server {
 			stream.println("STARTREQUEST");
 			stream.println(pGame.getServer().toString());
 			stream.println(pGame.getID().toString());
-		
+
 			app.getDTNSession().send(Client.GROUP_EID, DEFAULT_LIFETIME, buffer.toString());
-		
+
 			buffer.close();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		hostedGames.find(pGame).setState(GameState.STARTED);
 	}
-	
+
 	/**
 	 * inform clients about planning phase
 	 */
-	public void startPlanningPhase(GameInfo pGame)
+	public void startPlanningPhase(final GameInfo pGame)
 	{
+		if(!hostedGames.contains(pGame))
+		{
+			Log.e("Server", "Could not find game "+pGame+"!");
+			return;
+		}
+
 		final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		final PrintStream stream = new PrintStream(buffer);
 
@@ -100,73 +119,75 @@ public class Server {
 			stream.println("PLANNING");
 			stream.println(pGame.getServer().toString());
 			stream.println(pGame.getID().toString());
-		
+
 			app.getDTNSession().send(Client.GROUP_EID, DEFAULT_LIFETIME, buffer.toString());
-		
+
 			buffer.close();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		hostedGames.find(pGame).setState(GameState.PLANNING_PHASE);
 	}
-	
+
 	/**
 	 * get decisions from player
 	 * @param game
 	 * @param player
 	 * @param decisions
 	 */
-	public void receiveDecisions(GameInfo pGame, PlayerID pPlayer, DecisionContainer pDecisions)
+	public void receiveDecisions(final GameInfo pGame, final PlayerID pPlayer, final DecisionContainer pDecisions)
 	{
-		
+
 	}
-	
+
 	/**
 	 * distribute outcome to clients
 	 */
-	public void distributeOutcome(GameInfo pGame, OutcomeContainer outcome)
+	public void distributeOutcome(final GameInfo pGame, final OutcomeContainer outcome)
 	{
-		
+
 	}
-	
+
 	/**
 	 * player quits game
 	 * @param player
 	 */
-	public void playerAbort(GameInfo pGame, PlayerID pPlayer)
+	public void playerAbort(final GameInfo pGame, final PlayerID pPlayer)
 	{
 		if(pGame.getServer().equals(pPlayer))
 		{
 			announceNewServer(pGame, null);
 		}
 	}
-	
+
 	/**
 	 * server has quit, inform clients about new server
 	 * @param pGame old game info
 	 * @param pServer new server
 	 */
-	public void announceNewServer(GameInfo pGame, PlayerID pServer)
+	public void announceNewServer(final GameInfo pGame, final PlayerID pServer)
 	{
 		final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		final PrintStream stream = new PrintStream(buffer);
-		
+
 		try {
 			stream.println("NEWSERVER");
 			stream.println(pGame.getServer().toString());
 			stream.println(pGame.getID().toString());
 			stream.println(pServer.toString());
-		
+
 			app.getDTNSession().send(Client.GROUP_EID, DEFAULT_LIFETIME, buffer.toString());
-		
+
 			buffer.close();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public void handlePayload(DataInputStream input) throws IOException {		
+	public void handlePayload(final DataInputStream input) throws IOException {
 		final String command = input.readLine();
 
 		if(command.equals("JOIN"))
@@ -174,13 +195,23 @@ public class Server {
 			final PlayerID server = new PlayerID(input.readLine());
 			final GameID game = new GameID(input.readLine());
 			final PlayerID player = new PlayerID(input.readLine());
-			
+
 			if(server.equals(app.getPlayerID()))
 			{
 				addPlayer(new GameInfo(server, game), player);
 			}
 		}
 	}
-	
+
+	/**
+	 * @name lifetime constants
+	 * @{
+	 */
+	private static final int DEFAULT_LIFETIME = 100;
+	private static final int ANNOUNCEMENT_LIFETIME = DEFAULT_LIFETIME;
+	/**
+	 * @}
+	 */
+
 	private final AppActivity app;
 }
---
+++ app/src/org/beavers/gameplay/GameList.java
@@ -0,0 +1,69 @@
+package org.beavers.gameplay;
+
+import java.util.Hashtable;
+
+/**
+ * @author winniehell
+ * list for storing game information
+ */
+public class GameList {
+
+	/**
+	 * default constructor
+	 */
+	public GameList()
+	{
+		container = new Hashtable<String, GameInfo>();
+	}
+
+	/**
+	 * insert new game into list
+	 * @param pGame new game
+	 */
+	public void add(final GameInfo pGame)
+	{
+		container.put(pGame.toString(), pGame);
+	}
+
+	/**
+	 * @param pGame game to find
+	 * @return true if game is in list
+	 */
+	public boolean contains(final GameInfo pGame)
+	{
+		return container.containsKey(pGame.toString());
+	}
+
+	/**
+	 * find a game in the list
+	 * @param pGame game to find
+	 * @return game in list (or null)
+	 */
+	public GameInfo find(final GameInfo pGame)
+	{
+		return container.get(pGame.toString());
+	}
+
+	public GameInfo get(final int pIndex)
+	{
+		if(pIndex < container.size())
+		{
+			return container.get(container.keySet().toArray()[pIndex]);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * @return the number of elements in the list
+	 */
+	public int size()
+	{
+		return container.size();
+	}
+
+	/** underlying container */
+	private final Hashtable<String, GameInfo> container;
+}
