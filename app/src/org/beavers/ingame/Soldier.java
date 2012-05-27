5fde2aa4e8fceee81c6e84cb025d16ab340a6254
wintermadnezz <max.aj.franke@web.de>
2012-05-27 15:21:16 +0200

    @Manifest: Wird nun bevorzugt auf der SD-Karte installiert (ich hatte angeblich Speichermangel)
    @GameScene: zweiter Soldat hinzugefügt
    @Soldat: Soldaten können jetzt abwechselnd ausgewählt und separat
    gesteuert werden

--- app/src/org/beavers/ingame/Soldier.java
+++ app/src/org/beavers/ingame/Soldier.java
@@ -4,9 +4,6 @@ import org.anddev.andengine.entity.modifier.MoveModifier;
 import org.anddev.andengine.entity.modifier.RotationByModifier;
 import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.scene.Scene.IOnAreaTouchListener;
-import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
-import org.anddev.andengine.entity.scene.Scene.ITouchArea;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
 import org.anddev.andengine.input.touch.TouchEvent;
 import org.anddev.andengine.opengl.texture.TextureOptions;
@@ -16,39 +13,70 @@ import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.beavers.AppActivity;
 import org.beavers.gameplay.GameScene;
 
+
 public class Soldier extends GameObject  {
 	
 	private BitmapTextureAtlas mBitmapTextureAtlas;
 	private TiledTextureRegion playerTextureRegion;
 	private AnimatedSprite sprite;
 	private int speed=80;
+	private boolean isSelected=false;
+	private GameScene gscene;
+	private Soldier self;
 	
-	public Soldier(final AppActivity pApp,int team, int x, int y){
+	public Soldier(final AppActivity pApp,GameScene s, int team, int x, int y){
+		gscene=s;
+		self=this;
 		
 		if(team==0){
 			mBitmapTextureAtlas = new BitmapTextureAtlas(128,128,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 			BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 			playerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(mBitmapTextureAtlas, pApp, "96x96anim.png", 0, 0, 3, 2);
 			pApp.getEngine().getTextureManager().loadTexture(mBitmapTextureAtlas);
+			
 		}
 		else{}
 	}
-	
-	public void createSoldier(Scene s,int x, int y){
+	private Rectangle r;	
+	public void createSoldier(int x, int y){
 		sprite=new AnimatedSprite(x,y,playerTextureRegion){
 			@Override
 			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
-				this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2, pSceneTouchEvent.getY() - this.getHeight() / 2);
+				//this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2, pSceneTouchEvent.getY() - this.getHeight() / 2);
+				
+				if(isSelected==false){
+					
+					markSelected();
+					gscene.setSelectedSoldier(self);
+				}
+				else{
+					
+					markDeselected();
+					gscene.setSelectedSoldier(null);
+				}
+				
 				return true;
 			}
 		};
 		sprite.stopAnimation();
 		sprite.setRotationCenter(sprite.getWidth()/2, sprite.getHeight()/2);
-		s.attachChild(sprite);
+		gscene.attachChild(sprite);
 	}
 	
 	public AnimatedSprite getSprite(){
 		return sprite;
+	}
+	
+	public void markSelected(){
+		r= new Rectangle(sprite.getWidth()/2, sprite.getHeight()/2, 10, 10);
+		r.setColor(1,0 , 0);
+		sprite.attachChild(r);
+		isSelected=true;
+	}
+	
+	public void markDeselected(){
+		sprite.detachChild(r);
+		isSelected=false;
 	}
 	
 	public float[] getSceneCoordinates(){
