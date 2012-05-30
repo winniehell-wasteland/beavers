afbd0fa10a5bb33b572173d099c15e023b24f284
winniehell <git@winniehell.de>
2012-05-31 00:30:43 +0200

    remove old GameObject interface

--- app/src/org/beavers/ingame/GameObject.java
+++ app/src/org/beavers/ingame/GameObject.java
@@ -1,10 +1,5 @@
 package org.beavers.ingame;
 
-public abstract class GameObject {
-	public abstract String getType();
-	
-	public Position getPosition()
-	{
-		return null;
-	}
+public interface GameObject {
+
 }
